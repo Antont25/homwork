03d079a2c12a4e2908f4063cb390137e16b49094
Antont25 <toxafootbol1994@gmail.com>
2022-05-23 12:04:56 +0300

    "css"

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import style from './header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={style.leftSidebar}>
+            <div className={style.show}>Menu</div>
+            <div className={style.leftSidebarMenu}>
+                <NavLink to={'pre-junior'} className={style.item}>PRE-JUNIOR</NavLink>
+                <NavLink to={'junior'} className={style.item}>JUNIOR</NavLink>
+                <NavLink to={'junior-plus'} className={style.item}>JUNIOR-PLUS</NavLink>
+            </div>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPluse.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const JuniorPluse = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
